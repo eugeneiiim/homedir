67a4c07e38c90ad61f32e74967b02755265832f3
Eugene Marinelli <eugeneiiim@gmail.com>
2012-07-12 20:12:23 +0000

    setup_home performs additional setup

---
+++ bin/setup_home.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+sudo apt-get install ruby htop man subversion screen
+
+# Oh-my-zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+git checkout .zshrc  # OMZ changes .zshrc -- revert it.
+
+# scala-mode
+SMDIR=~/emacs_stuff/scala-mode
+svn co http://lampsvn.epfl.ch/svn-repos/scala/scala-tool-support/trunk/src/emacs/ $SMDIR
+make -C $SMDIR
