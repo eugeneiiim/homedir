354beed4b8e897cdc4b63e035bf1010ad3d7bdd4
Eugene Marinelli <eugeneiiim@gmail.com>
2012-07-12 23:42:51 +0000

    install elasticsearch, yas, hbase

--- bin/setup_home.sh
+++ bin/setup_home.sh
@@ -14,5 +14,16 @@ make -C $SMDIR
 # Blend
 git clone git@github.com:blendlabs/treat.git
 
-wget http://archive.cloudera.com/cdh4/cdh/4/hbase-0.92.1-cdh4.0.0.tar.gz
-tar xzfv hbase-0.92.1-cdh4.0.0.tar.gz
+HBASE_PKG=hbase-0.92.1-cdh4.0.0.tar.gz
+wget http://archive.cloudera.com/cdh4/cdh/4/$HBASE_PKG
+tar xzfv $HBASE_PKG
+
+ES_PKG=elasticsearch-0.19.8
+wget https://github.com/downloads/elasticsearch/elasticsearch/$ES_PKG.tar.gz
+tar xvfz $ES_PKG.tar.gz
+ln -s $ES_PKG elasticsearch
+
+# yasnippet
+YDIR=~.emacs.d/plugins
+mkdir $YDIR
+git clone git://github.com/capitaomorte/yasnippet.git $YDIR/yasnippet
