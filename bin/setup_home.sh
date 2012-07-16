1eb6508e180a4df5dda9c2142dedea80dfecc514
Eugene Marinelli <eugeneiiim@gmail.com>
2012-07-16 10:31:45 +0000

    setup_home installs redis

--- bin/setup_home.sh
+++ bin/setup_home.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-sudo apt-get install ruby htop man subversion screen
+sudo apt-get install ruby htop man subversion screen redis-server
 
 # Oh-my-zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
