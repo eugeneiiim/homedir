3017d14e9260ec78b576578a577dbf6ec860b956
Eugene Marinelli <eugeneiiim@gmail.com>
2012-07-24 07:41:59 +0000

    include unadded files

--- bin/common.rb
+++ bin/common.rb
@@ -1,3 +1,7 @@
+def unstaged_modified_files
+  `git ls-files --modified --other --exclude-standard`.split("\n")
+end
+
 def git_do_on_starting_with(command, pattern, all_filenames)
   toadd = all_filenames.select {|f| f.split('/').last.start_with?(pattern) }
   if toadd.empty?
