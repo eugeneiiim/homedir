de5adbb04f24714bedc50a38e99ccd6645523c15
Eugene Marinelli <eugeneiiim@gmail.com>
2012-07-25 04:06:53 +0000

    Array.exists? method

--- bin/common.rb
+++ bin/common.rb
@@ -2,15 +2,16 @@ def unstaged_modified_files
   `git ls-files --modified --other --exclude-standard`.split("\n")
 end
 
+class Array
+  def exists?(&fn)
+    !self.select(&fn).empty?
+  end
+end
+
 def git_do_on_starting_with(command, patterns, all_filepaths)
-  toadd = all_filepaths.select do |f|
-    filename = f.split('/').last
-
-    matching_patterns = patterns.select do |pattern|
-      filename.start_with?(pattern)
-    end
-
-    !matching_patterns.empty?
+  toadd = all_filepaths.select do |filepath|
+    filename = filepath.split('/').last
+    patterns.exists? { |pattern| filename.start_with?(pattern) }
   end
 
   if toadd.empty?
