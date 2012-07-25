0c834b646f03ae7f21badc5f49531a15142dad63
Eugene Marinelli <eugeneiiim@gmail.com>
2012-07-25 04:01:39 +0000

    support multiple patterns in gap/gcop/grp

--- bin/common.rb
+++ bin/common.rb
@@ -2,8 +2,17 @@ def unstaged_modified_files
   `git ls-files --modified --other --exclude-standard`.split("\n")
 end
 
-def git_do_on_starting_with(command, pattern, all_filenames)
-  toadd = all_filenames.select {|f| f.split('/').last.start_with?(pattern) }
+def git_do_on_starting_with(command, patterns, all_filepaths)
+  toadd = all_filepaths.select do |f|
+    filename = f.split('/').last
+
+    matching_patterns = patterns.select do |pattern|
+      filename.start_with?(pattern)
+    end
+
+    !matching_patterns.empty?
+  end
+
   if toadd.empty?
     puts 'No matching files.'
   else
