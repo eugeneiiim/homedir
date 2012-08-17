7590cdd3f550f997ce0c6eb351952799a5d38105
Eugene Marinelli <eugeneiiim@gmail.com>
2012-08-17 11:43:13 +0000

    include py and js

--- emacs_stuff/find-file-in-project.el
+++ emacs_stuff/find-file-in-project.el
@@ -70,7 +70,7 @@
 (defvar ffip-regexp
   (concat ".*\\.\\("
 	  (mapconcat (lambda (x) x)
-		     '("rb" "coffee" "scala" "sh" "css" "jst" "erb" "html" "el" "less") "\\|")
+		     '("rb" "coffee" "scala" "sh" "css" "jst" "js" "erb" "html" "el" "less" "py") "\\|")
 	  "\\)")
   "Regexp of things to look for when using find-file-in-project.")
 
