33d59313dae48e684f9ce8ee72c88cacaec5c4b0
Eugene Marinelli <eugeneiiim@gmail.com>
2012-09-07 18:22:09 -0500

    asdf

--- emacs_stuff/find-file-in-project.el
+++ emacs_stuff/find-file-in-project.el
@@ -70,7 +70,7 @@
 (defvar ffip-regexp
   (concat ".*\\.\\("
 	  (mapconcat (lambda (x) x)
-		     '("rb" "coffee" "scala" "sh" "css" "jst" "js" "erb" "html" "el" "less" "py" "avdl") "\\|")
+		     '("rb" "coffee" "scala" "sh" "css" "jst" "js" "erb" "html" "el" "less" "py" "avdl" "routes") "\\|")
 	  "\\)")
   "Regexp of things to look for when using find-file-in-project.")
 
