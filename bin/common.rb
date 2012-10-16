6324b6bc3a9cf24fbf69b211a6d3fbf27a5c813d
Eugene Marinelli <eugeneiiim@gmail.com>
2012-10-15 19:30:54 -0500

    grm script

--- bin/common.rb
+++ bin/common.rb
@@ -2,13 +2,21 @@ def unstaged_modified_files
   `git ls-files --modified --other --exclude-standard`.split("\n")
 end
 
+def staged_files
+  `git diff --cached --name-only`.split("\n")
+end
+
+def all_git_files
+  `git ls-tree -r --name-only HEAD`
+end
+
 class Array
   def exists?(&fn)
     !self.select(&fn).empty?
   end
 end
 
-def git_do_on_starting_with(command, patterns, all_filepaths)
+def do_on_starting_with(command, patterns, all_filepaths)
   toadd = all_filepaths.select do |filepath|
     filename = filepath.split('/').last
     patterns.exists? { |pattern| filename.start_with?(pattern) }
@@ -17,7 +25,7 @@ def git_do_on_starting_with(command, patterns, all_filepaths)
   if toadd.empty?
     puts 'No matching files.'
   else
-    cmd = "git #{command} #{toadd.join(' ')} ; git status"
+    cmd = "#{command} #{toadd.join(' ')} ; git status"
     puts cmd
     system(cmd)
   end
