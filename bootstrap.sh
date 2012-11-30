39540e32cc3116ede25eb188e27b90011d5d5d4c
Eugene Marinelli <eugeneiiim@gmail.com>
2012-11-29 20:40:55 -0800

    no verify ssl in setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ sudo apt-get -y upgrade
 sudo apt-get -y install git
 
 rm -rf homedir
+git config --global http.sslVerify false
 git clone git@github.com:eugeneiiim/homedir.git
 shopt -s dotglob # make dotfiles moveable
 rsync -a homedir/ ~
