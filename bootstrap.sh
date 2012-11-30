8d11914640cd468eeb39340c7b1c55cd22720f7c
Eugene Marinelli <eugeneiiim@gmail.com>
2012-11-29 20:42:56 -0800

    clone via http

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ sudo apt-get -y install git
 
 rm -rf homedir
 git config --global http.sslVerify false
-git clone git@github.com:eugeneiiim/homedir.git
+git clone https://github.com/eugeneiiim/homedir
 shopt -s dotglob # make dotfiles moveable
 rsync -a homedir/ ~
 bin/setup/setup_home.sh
