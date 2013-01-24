dd35d28fa8fc692ca4a7b015fa0eade9ab95b9c3
Eugene Marinelli <eugeneiiim@gmail.com>
2013-01-24 15:38:13 -0800

    simple email sender script

---
+++ bin/sendmail.rb
@@ -0,0 +1,31 @@
+#! /usr/bin/ruby
+
+require 'rubygems'
+require 'pony'
+
+if ARGV.length < 3
+  puts 'args: to subject body'
+  exit
+end
+
+to, subject, body = ARGV
+
+File.open('/home/emarinelli/.sendmail', 'r').readlines.each do |l|
+  eval(l.strip)
+end
+
+Pony.mail({
+  :to => to,
+  :subject => subject,
+  :body => body,
+  :via => :smtp,
+  :via_options => {
+    :address              => 'smtp.gmail.com',
+    :port                 => '587',
+    :enable_starttls_auto => true,
+    :user_name            => SENDER_GMAIL,
+    :password             => SENDER_GMAIL_PASSWORD,
+    :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+    :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+  }
+})
